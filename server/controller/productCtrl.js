06b588808f7d3b5f459c0ace52bcf8916152556e
Sathsara Wijekulasuriya <sathsara2000@gmail.com>
2023-07-02 20:26:06 +0530

    Nodemailer

--- server/controller/productCtrl.js
+++ server/controller/productCtrl.js
@@ -42,14 +42,48 @@ export const getProduct = asyncHandler(async(req,res)=>{
 
 //GET ALL PRODUCTS
 export const getAllProducts = asyncHandler(async(req,res)=>{
-    const queryObj = req.query;
-    const excludeFields = ["page", "sort","limit","fields"]
-    //TODO
+    
+    
     try {
-        const allProducts = await productModel.where("category").equals(
-            req.query.category,
-        );
-        res.json(allProducts);
+
+        //Filtering
+        const queryObj = {...req.query};
+        const excludeFields = ["page", "sort","limit","fields"];
+        excludeFields.forEach((el) => delete queryObj[el]);
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+        let query = productModel.find(JSON.parse(queryStr));
+
+        //Sorting
+        if (req.query.sort){
+           const sortBy = req.query.sort.split(',').join(" ");
+           query = query.sort(sortBy);
+        }else{
+           query = query.sort("-createdAt");
+        }
+
+        //Limiting the fields
+        if(req.query.fields){
+            const fields = req.query.sort.fields(',').join(" ");
+            query = query.sort(fields);
+        }else{
+            query = query.select("-__v");
+        }
+
+        //Pagination
+        const page = req.query.page;
+        const limit = req.query.limit;
+        const skip = (page - 1)*limit;
+        query = query.skip(skip).limit(limit);
+
+        if(req.query.page){
+            const productCount = await productModel.countDocuments();
+            if(skip >= productCount) throw new Error("This page does not exists");
+        }
+
+        const product = await query;
+        res.json(product);
     } catch (error) {
         throw new Error('Error occures when fetching all products');
     }
